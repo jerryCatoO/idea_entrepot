ed31b468d4dd92ef10a7cc64644435a189bbd2d3
JerryCatoO <523272426@qq.com>
2019-09-26 21:39:10 +0800

    上传

---
+++ abc/src/main/java/cn/itheima/dao/AccountMapper.java
@@ -0,0 +1,17 @@
+package cn.itheima.dao;
+
+import cn.itheima.domain.Account;
+
+public interface AccountMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Account record);
+
+    int insertSelective(Account record);
+
+    Account selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Account record);
+
+    int updateByPrimaryKey(Account record);
+}
